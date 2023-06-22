33d40e37b369a4b617f9f16d901e22d09255bccc
newisdartagnan <borisikula@gmail.com>
2023-06-22 03:06:00 -0700

    last

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
-file { '/home/ubuntu/.ssh/config':
+file { '/root/.ssh/config':
   ensure  => present,
-  content => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no\n",
+  content => "Host *\n  PasswordAuthentication no\n  SendEnv LANG LC_*\n  HashKnownHosts yes\n  GSSAPIAuthentication yes\n  GSSAPIDelegateCredentials no\n  IdentityFile ~/.ssh/school\n",
   owner   => 'ubuntu',
   group   => 'ubuntu',
   mode    => '0644',
