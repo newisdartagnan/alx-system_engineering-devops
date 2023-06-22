bbeb85114e687c0bc454cbc52c3de61f7e678c7f
Boris Ikula <111108765+newisdartagnan@users.noreply.github.com>
2023-06-22 22:03:19 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,7 +0,0 @@
-# Handles 500 apache error
-
-exec { '/var/www/html/wp-settings.php':
-  command => "sed -i 's/\\/class-wp-locale.phpp/\\/class-wp-locale.php/g'\
-	      /var/www/html/wp-settings.php",
-  path    => '/bin/',
-}
